cce7d7b12e59a64f5a3ca2a594cfe9c9eae4c314
Andreas Roos <156186841+Gaurgle@users.noreply.github.com>
2025-01-23 22:44:53 +0100

    Update Webshop_6_Sensitive data READ.sql

--- Webshop 1.3/Webshop_6_Sensitive data READ.sql
+++ Webshop 1.3/Webshop_6_Sensitive data READ.sql
@@ -6,14 +6,14 @@ GRANT ALL PRIVILEGES ON sensitive_data.* TO 'Admin'@'localhost';
 FLUSH PRIVILEGES;
 
 
--- as BLOB, inte dekrypterad
+-- as BLOB, not decrypted
 SELECT 
     password, 
     AES_DECRYPT(credit_card_encrypted, LEFT(SHA2('enter_password', 256), 16)) AS decrypted_cc
 FROM sensitive_data.credentials
 WHERE customer_id = 1;
 
--- dekrypterat:
+-- decrypted:
 SELECT 
     password, 
     CAST(AES_DECRYPT(credit_card_encrypted, LEFT(SHA2('MaLong', 256), 16)) AS CHAR) AS decrypted_cc
@@ -26,7 +26,7 @@ SELECT
 FROM sensitive_data.credentials
 WHERE customer_id = 3;
 
--- alla customers credentials (BLOB):
+-- all customers credentials (BLOB):
 SELECT 
 	webshop.customers.first_name as first_name, 
 	customers.last_name as last_name, 
@@ -37,7 +37,7 @@ JOIN sensitive_data.credentials
 	ON sensitive_data.credentials.credential_id = webshop.customers.customer_id;
     
     
--- alla customers credentials dekryptrat
+-- all customers credentials decrypted:
 SELECT 
 webshop.customers.first_name AS first_name, 
 webshop.customers.last_name AS last_name, 
